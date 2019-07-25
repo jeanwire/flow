80ec7d039c07394fea5b8e8e597a14c9b6aeb0ca
Kathleen McGuire <kathleen.macg@gmail.com>
2019-07-25 12:42:13 -0400

    converted various lists to sets if indexing of the lists was not required, as locating items in sets is faster

--- builder.py
+++ builder.py
@@ -14,7 +14,7 @@ def main():
         print(mino)
 
         if (len(mino[0]) <= board.size):
-            temp_board, inserted, mino_loc = board.insert(mino)
+            temp_board, inserted = board.insert(mino)
 
     board.game = copy.deepcopy(temp_board)
     for line in board.game:
@@ -34,10 +34,7 @@ class Board(object):
         self.game = []
         for i in range(size):
             self.game.append([None] * size)
-        self.visited = {}
-        for i in range(size):
-            for j in range(size):
-                self.visited[(i, j)] = False
+        self.visited = set()
         self.complete = False
         self.unused_colors = ['m', 'p', 'a', 'o', 'y', 'b', 'g', 'r']
 
@@ -50,20 +47,20 @@ class Board(object):
         temp, valid = self.mino_in_board(mino, row, col, color)
 
 
-        if (not valid and row + 1 < self.size - len(mino)):
+        if (isinstance(valid, tuple) and not valid[0] and row + 1 < self.size - len(mino)):
             temp, valid = self.mino_in_board(mino, row + 1, col, color)
-        elif (not valid and col + 1 < self.size - len(mino[0])):
+        elif (isinstance(valid, tuple) and not valid[0] and col + 1 < self.size - len(mino[0])):
             temp, valid = self.mino_in_board(mino, row, col + 1, color)
-        elif (not valid and row - 1 >= 0):
+        elif (isinstance(valid, tuple) and not valid[0] and row - 1 >= 0):
             temp, valid = self.mino_in_board(mino, row - 1, col, color)
-        elif (not valid and col - 1 >= 0):
+        elif (isinstance(valid, tuple) and not valid[0] and col - 1 >= 0):
             temp, valid = self.mino_in_board(mino, row, col - 1, color)
-        return (temp, valid, (row, col))
+        return (temp, valid)
 
 
     def mino_in_board(self, mino, row, col, color):
         temp = copy.deepcopy(self.game)
-        temp_visited = []
+        temp_visited = set()
 
         mino_row = 0
         for i in range(row, row + len(mino)):
@@ -71,19 +68,19 @@ class Board(object):
             for j in range(col, col + len(mino[0])):
                 if (mino[mino_row][mino_col] != 'O'):
                     temp[i][j] = re.sub('X', color, mino[mino_row][mino_col])
-                    temp_visited.append((i, j))
+                    temp_visited.add((i, j))
                 mino_col += 1
             mino_row += 1
 
         valid = self.validate_board(temp, temp_visited)
         if (valid):
             for sq in temp_visited:
-                self.visited[sq] = True
+                self.visited.add(sq)
         return (temp, valid)
 
 
     def onion(self):
-        """Draws a line that traces along the lines present in the board"""
+        """Draws a line that traces along the lines already present in the board"""
 
         board = copy.deepcopy(self.game)
         one_neighbor = set()
@@ -101,7 +98,7 @@ class Board(object):
                 elif len(board[i][j]) == 2:
                     endpoints.add((i, j))
 
-        pot_endpoints = one_neighbor
+        pot_endpoints = one_neighbor.copy()
         for endpoint in endpoints:
             pot_endpoints = pot_endpoints.union(self.ortho_neighbors(endpoint, board))
 
@@ -115,9 +112,8 @@ class Board(object):
         # marking this as an endpoint
         board[curr_point[0]][curr_point[1]] = color + color
 
-        # randomly ends after path is 3 sqs long and <= 10 sqs long
-        # does this generate a different number each loop?
-        for i in range(0, 5):
+        # randomly ends after path is 3 sqs long and <= 8 sqs long
+        while (len(path) < 3 or random.randint(0, 8 - len(path))):
             empty_neighbors = self.ortho_neighbors(curr_point, board)
             print(empty_neighbors)
             # if there is only one neighbor, don't need to perform any further analysis
@@ -145,10 +141,18 @@ class Board(object):
 
         board[curr_point[0]][curr_point[1]] = color + color
 
+        # if line is only 2 sqs long, need to backtrack
+
+
+        valid = self.validate_board(board, path.union(self.visited))
+        print("valid? ", valid)
+
         return board
 
 
     def validate_board(self, board, visited):
+        """Determines whether a board contains any isolated groups of fewer
+        than 3 squares"""
         clusters = []
 
         checked = visited.copy()
@@ -157,59 +161,51 @@ class Board(object):
         while(len(checked) < self.size * self.size):
             # need to do this in one loop so we can break out of it
             for i in range(self.size * self.size):
-                try:
-                    checked.index((i // 5, i % 5))
-                except ValueError:
+                if (i // 5, i % 5) not in checked:
                     first_sq = (i // 5, i % 5)
                     break;
 
-            cluster = [first_sq]
-            self.bfs(board, cluster, cluster)
+            cluster = set({first_sq})
+            self.bfs(board, cluster.copy(), cluster.copy())
             if cluster:
                 clusters.append(cluster)
-                checked += cluster
+                for sq in cluster:
+                    checked.add(sq)
             # reset this value
             first_sq = None
 
         for cluster in clusters:
             if len(cluster) <= 2:
-                return False
+                return False, cluster
 
         return True
 
     def bfs(self, board, sqs, cluster):
-        children = []
+        children = set()
         for sq in sqs:
             if (sq[0] != 0 and not board[sq[0] - 1][sq[1]]):
-                try:
-                    cluster.index((sq[0] - 1, sq[1]))
-                except ValueError:
-                    children.append((sq[0] - 1, sq[1]))
-                    cluster.append((sq[0] - 1, sq[1]))
+                if (sq[0] - 1, sq[1]) not in cluster:
+                    children.add((sq[0] - 1, sq[1]))
+                    cluster.add((sq[0] - 1, sq[1]))
             if (sq[0] != self.size - 1 and not board[sq[0] + 1][sq[1]]):
-                try:
-                    cluster.index((sq[0] + 1, sq[1]))
-                except ValueError:
-                    children.append((sq[0] + 1, sq[1]))
-                    cluster.append((sq[0] + 1, sq[1]))
+                if (sq[0] + 1, sq[1]) not in cluster:
+                    children.add((sq[0] + 1, sq[1]))
+                    cluster.add((sq[0] + 1, sq[1]))
             if (sq[1] != 0 and not board[sq[0]][sq[1] - 1]):
-                try:
-                    cluster.index((sq[0], sq[1] - 1))
-                except:
-                    children.append((sq[0], sq[1] - 1))
-                    cluster.append((sq[0], sq[1] - 1))
+                if (sq[0], sq[1] - 1) not in cluster:
+                    children.add((sq[0], sq[1] - 1))
+                    cluster.add((sq[0], sq[1] - 1))
             if (sq[1] != self.size - 1 and not board[sq[0]][sq[1] + 1]):
-                try:
-                    cluster.index((sq[0], sq[1] + 1))
-                except:
-                    children.append((sq[0], sq[1] + 1))
-                    cluster.append((sq[0], sq[1] + 1))
+                if (sq[0], sq[1] + 1) not in cluster:
+                    children.add((sq[0], sq[1] + 1))
+                    cluster.add((sq[0], sq[1] + 1))
 
         if (len(children)):
             self.bfs(board, children, cluster)
 
 
     def ortho_neighbors(self, point, board):
+        """Finds the empty neighbors that are orthogonal to the present square"""
         neighbors = set()
         if point[0] != 0:
             if not board[point[0] - 1][point[1]]:
@@ -227,6 +223,9 @@ class Board(object):
         return neighbors
 
     def filled_neighbors(self, point, board, path):
+        """Finds the filled neighbors of the square, including orthogonal and
+        diagonal neighbors. Only counts neighbors if they are not in the line
+        currently being drawn"""
         neighbors = []
         print("path: ", path)
         # row above
