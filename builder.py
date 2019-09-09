eb5f7edb51d55d12214550ee0eb503b4db48d4d8
Kathleen McGuire <kathleen.macg@gmail.com>
2019-09-09 17:34:15 -0400

    made temporary board a class variable to avoid having to pass the board around to different methods

--- builder.py
+++ builder.py
@@ -3,58 +3,22 @@ import json
 import random
 import re
 import copy
+import stack
 
 
 def main():
     minos_dict = import_minos()
-    for _ in range(100):
-        board = Board(5)
-        inserted = False
-        while (not inserted):
-            mino = copy.deepcopy(select_mino(minos_dict))
 
-            if (len(mino[0]) <= board.size):
-                temp_board, inserted = board.insert(mino)
-
-        board.game = copy.deepcopy(temp_board)
-        num_line = 1
-        while (not board.complete):
-            board.draw_line(num_line)
-            num_line += 1
-            if num_line > 100:
-                print("BAD BOARD")
-                break
-
-        for line in board.game:
-            print(line)
-
-        print(" ")
+    starter_board = Board(5, minos_dict)
 
     # when board is completed, write to file in json format
-# a thought journey: making the board a grid of Points
-# class Point():
-#     def __init__(self, x, y, size = 5):
-#         this.x = x
-#         this.y = y
-#         self.size = size
-#         self.valid = x < size -1 and x >=0 and y < size - 1 and y >= 0
-#     def __eq__(self, p2):
-#         return this.x == p2.x and this.y == p2.y
-#     def __hash__(self):
-#         return hash((this.x, this.y))
-#     def orthogonal(self):
-#         ret = [Point(x - 1, y), Point(x + 1, y), Point(x, y - 1), Point(x, y + 1)]
-#         return [r for r in ret if r.valid]
-#     def all_neighbors(self):
-#         ret = [Point(x - 1, y + 1), Point(x, y + 1), Point(x + 1, y + 1),
-#         Point(x - 1, y), Point(x + 1, y), Point(x - 1, y - 1), Point(x, y - 1),
-#         Point(x + 1, y - 1)]
-#         return [r for r in ret if r.valid]
+
 
 class Board(object):
-    def __init__(self, size):
+    def __init__(self, size, minos_dict):
         self.size = size
         self.game = [[None for _ in range(size)] for _ in range(size)]
+        self.temp_game = copy.deepcopy(self.game)
         self.paths = []
         self.complete = False
         self.unused_colors = ['m', 'p', 'c', 'o', 'y', 'b', 'g', 'r']
@@ -65,28 +29,51 @@ class Board(object):
         self.extremities = set(extremities)
         self.corners = set([(0, 0), (0, size - 1), (size - 1, 0), (size - 1, size - 1)])
 
+        inserted = False
+        while not inserted:
+            mino = copy.deepcopy(select_mino(minos_dict))
+
+            if (len(mino[0]) <= self.size):
+                inserted = self.insert(mino)
+
+        # take the mino path and put it on the stack
+        # will probably change this to a more stack-centric method at some other point
+        self.tree = stack.Tree(self.paths[0])
+        for i in range(0,5):
+            self.draw_line()
+            # self.validate_line()
+            # valid = self.validate_board()
+            # if not isinstance(valid, set)
+
+        print(self)
+
+
+    def __str__(self):
+        return "\n".join([" ".join(str(item) for item in row) for row in self.game])
+
 
     def insert(self, mino):
         row = random.randint(0, self.size - len(mino))
         col = random.randint(0, self.size - len(mino[0]))
 
         color = self.unused_colors.pop()
-        temp, valid = self.mino_in_board(mino, row, col, color)
-
-
+        valid = self.mino_in_board(mino, row, col, color)
         if (isinstance(valid, set) and row + 1 < self.size - len(mino)):
-            temp, valid = self.mino_in_board(mino, row + 1, col, color)
+            valid = self.mino_in_board(mino, row + 1, col, color)
         elif (isinstance(valid, set) and col + 1 < self.size - len(mino[0])):
-            temp, valid = self.mino_in_board(mino, row, col + 1, color)
+            valid = self.mino_in_board(mino, row, col + 1, color)
         elif (isinstance(valid, set) and row - 1 >= 0):
-            temp, valid = self.mino_in_board(mino, row - 1, col, color)
+            valid = self.mino_in_board(mino, row - 1, col, color)
         elif (isinstance(valid, set) and col - 1 >= 0):
-            temp, valid = self.mino_in_board(mino, row, col - 1, color)
-        return (temp, valid)
+            valid = self.mino_in_board(mino, row, col - 1, color)
+
+        if not isinstance(valid, set):
+            self.game = copy.deepcopy(self.temp_game)
+        return valid
 
 
     def mino_in_board(self, mino, row, col, color):
-        temp = copy.deepcopy(self.game)
+        self.temp_game = copy.deepcopy(self.game)
         temp_visited = set()
         endpoints = set()
 
@@ -95,7 +82,7 @@ class Board(object):
             mino_col = 0
             for j in range(col, col + len(mino[0])):
                 if (mino[mino_row][mino_col] != 'O'):
-                    temp[i][j] = re.sub('X', color, mino[mino_row][mino_col])
+                    self.temp_game[i][j] = re.sub('X', color, mino[mino_row][mino_col])
                     if (len(mino[mino_row][mino_col]) == 2):
                         endpoints.add((i, j))
                     else:
@@ -103,12 +90,12 @@ class Board(object):
                 mino_col += 1
             mino_row += 1
 
-        valid = self.validate_board(temp, temp_visited.union(endpoints))
+        valid = self.validate_board(temp_visited.union(endpoints))
 
         if (not isinstance(valid, set)):
             path = self.mino_to_line(temp_visited, endpoints)
             self.paths.append(path)
-        return (temp, valid)
+        return valid
 
 
     def mino_to_line(self, mino_sqs, endpts):
@@ -128,30 +115,30 @@ class Board(object):
         return path
 
 
-    def draw_line(self, num_line):
-        board = copy.deepcopy(self.game)
+    def draw_line(self):
+        self.temp_game = copy.deepcopy(self.game)
+        board = self.temp_game
         color = self.unused_colors.pop()
         path = []
 
-        pot_endpoints = self.find_pot_ends(board)
+        pot_endpoints = self.find_pot_ends()
 
         if pot_endpoints:
             curr_point = pot_endpoints.pop()
         else:
-            for line in board:
-                print(line)
+            print(self)
+            # for debugging: will print the board then crash
             curr_point = pot_endpoints.pop()
         path.append(curr_point)
         board[curr_point[0]][curr_point[1]] = color + color
-        # if the endpoint is on the edge and is not the first line, it has a 1/4 chance of moving along the edge
-        # if num_line > 1 and (curr_point[0] == 0 or curr_point[0] == self.size - 1 or curr_point[1] == 0 or curr_point[1] == self.size - 1):
-        if num_line > 1 and curr_point in self.extremities:
+        # if the endpoint is on the edge, it has a 1/4 chance of moving along the edge
+        if curr_point in self.extremities:
             if random.random() < 0.25:
-                self.game = self.edge(path, board, color)
+                self.edge(path, color)
             else:
-                self.game = self.onion(path, board, color)
+                self.onion(path, color)
         else:
-            self.game = self.onion(path, board, color)
+            self.onion(path, color)
 
         total_sqs_filled = 0
         for path in self.paths:
@@ -160,15 +147,15 @@ class Board(object):
             self.complete = True
 
 
-    def edge(self, path, board, color):
-        print("EDGE LINE")
+    def edge(self, path, color):
         curr_point = path[0]
         horizontal = True
         vertical = False
         side = horizontal
         direction = 0
+        board = self.temp_game
 
-        neighbors = self.ortho_neighbors(curr_point, board)
+        neighbors = self.ortho_neighbors(curr_point)
         for neighbor in neighbors:
             if neighbor in self.extremities:
                 if (neighbor[0] < curr_point[0]):
@@ -248,22 +235,23 @@ class Board(object):
                         break
 
         board[curr_point[0]][curr_point[1]] *= 2
-        board = self.validate_line(board, path, color)
-        return board
+        self.validate_line(path, color)
 
 
-    def onion(self, path, board, color):
+    def onion(self, path, color):
         """Draws a line that traces along the lines already present in the board.
         The line may trace along the edges if it cannot trace any further along
         the already present lines."""
         curr_point = path[0]
+        board = self.temp_game
         # randomly ends after path is 3 sqs long and <= 9 sqs long
-        while (len(path) < 3 or random.randint(0, 9 - len(path))):
-            empty_neighbors = self.ortho_neighbors(curr_point, board)
+        # while (len(path) < 3 or random.randint(0, 9 - len(path))):
+        while (len(path) < random.randint(3, max(3, 9 - len(path)))):
+            empty_neighbors = self.ortho_neighbors(curr_point)
             # if there is only one neighbor, need to make sure line isn't doubling back
             if len(empty_neighbors) == 1:
                 point = empty_neighbors.pop()
-                if len(self.filled_neighbors(point, board, path)[1]) < 3:
+                if len(self.filled_neighbors(point, path)[1]) < 3:
                     curr_point = point
                     path.append(curr_point)
                     board[curr_point[0]][curr_point[1]] = color
@@ -273,7 +261,7 @@ class Board(object):
             # allows for "onion" behavior, which is a sq that contains filled neighbors
             elif empty_neighbors:
                 for neighbor in empty_neighbors:
-                    if self.filled_neighbors(neighbor, board, path)[0]:
+                    if self.filled_neighbors(neighbor, path)[0]:
                         curr_point = neighbor
                         path.append(curr_point)
                         board[curr_point[0]][curr_point[1]] = color
@@ -283,46 +271,46 @@ class Board(object):
                 break
 
         board[curr_point[0]][curr_point[1]] = color + color
-        board = self.validate_line(board, path, color)
+        self.validate_line(path, color)
         return board
 
 
-    def validate_line(self, board, path, color):
+    def validate_line(self, path, color):
         # if line is only 2 sqs long, need to extend
+        board = self.temp_game
         if len(path) < 3:
-            self.extend_path(board, path, color)
+            self.extend_path(path, color)
 
-        valid = self.validate_board(board, path)
+        valid = self.validate_board(path)
 
         needs_rollback = False
         # if line has left 1 or 2 sqs open adjacent to the endpoint, need to extend the line
         # if open sqs are not adjacent to an endpoint, need to remove sqs from the path
         while isinstance(valid, set):
-            hole_filled = self.fill_holes(board, path, valid, color)
+            hole_filled = self.fill_holes(path, valid, color)
             if not hole_filled:
                 needs_rollback = True
                 break
-            valid = self.validate_board(board, path)
+            valid = self.validate_board(path)
 
         while (needs_rollback and len(path) > 3):
-            self.rollback(board, path)
-            valid = self.validate_board(board, path)
+            self.rollback(path)
+            valid = self.validate_board(path)
             if not isinstance(valid, set):
                 needs_rollback = False
 
         # adding the line as it was randomly drawn may not be possible
         if (not needs_rollback and len(path) > 3):
             self.paths.append(path)
-            print(path)
-            return board
-
-        self.unused_colors.append(color)
-        return self.game
+            self.game = copy.deepcopy(self.temp_game)
+        else:
+            self.unused_colors.append(color)
 
 
-    def validate_board(self, board, path):
+    def validate_board(self, path):
         """Determines whether a board contains any isolated groups of fewer
         than 3 squares. Returns true or a set of 1 or 2 isolated squares"""
+        board = self.temp_game
         clusters = []
 
         checked = set()
@@ -342,7 +330,7 @@ class Board(object):
                     break;
 
             cluster = set({first_sq})
-            self.bfs(board, cluster.copy(), cluster)
+            self.bfs(cluster.copy(), cluster)
             if cluster:
                 clusters.append(cluster)
                 for sq in cluster:
@@ -356,7 +344,8 @@ class Board(object):
 
         return True
 
-    def bfs(self, board, sqs, cluster):
+    def bfs(self, sqs, cluster):
+        board = self.temp_game
         children = set()
         for sq in sqs:
             if (sq[0] != 0 and not board[sq[0] - 1][sq[1]]):
@@ -377,11 +366,12 @@ class Board(object):
                     cluster.add((sq[0], sq[1] + 1))
 
         if (len(children)):
-            self.bfs(board, children, cluster)
+            self.bfs(children, cluster)
 
 
-    def ortho_neighbors(self, point, board):
+    def ortho_neighbors(self, point):
         """Finds the empty neighbors that are orthogonal to the present square"""
+        board = self.temp_game
         neighbors = set()
         if point[0] != 0:
             if not board[point[0] - 1][point[1]]:
@@ -398,11 +388,12 @@ class Board(object):
 
         return neighbors
 
-    def filled_neighbors(self, point, board, path_list):
+    def filled_neighbors(self, point, path_list):
         """Finds the filled neighbors of the square, including orthogonal and
         diagonal neighbors. Counts the neighbors within the line passed as an
         argument separately from the other neighbors, which is used to prevent
         the lines doubling back upon themselves"""
+        board = self.temp_game
         path = set()
         for sq in path_list:
             path.add(sq)
@@ -458,14 +449,14 @@ class Board(object):
         return (filled_neighbors, own_neighbors)
 
 
-    def rollback(self, board, path):
+    def rollback(self, path):
         last_sq = path.pop();
-        board[last_sq[0]][last_sq[1]] = None;
+        self.temp_game[last_sq[0]][last_sq[1]] = None;
         last_sq = path[len(path) - 1]
-        board[last_sq[0]][last_sq[1]] *= 2;
+        self.temp_game[last_sq[0]][last_sq[1]] *= 2;
 
 
-    def find_pot_ends(self, board):
+    def find_pot_ends(self):
         """Builds up a set of empty squares on the board that either only have 1
         neighbor or are neighbors of the current endpoints"""
         one_neighbor = set()
@@ -473,59 +464,65 @@ class Board(object):
 
         for i in range(self.size):
             for j in range(self.size):
-                if not board[i][j]:
-                    neighbors = self.ortho_neighbors((i, j), board)
+                if not self.temp_game[i][j]:
+                    neighbors = self.ortho_neighbors((i, j))
                     if len(neighbors) == 1:
                         one_neighbor.add((i, j))
-                elif len(board[i][j]) == 2:
+                elif len(self.temp_game[i][j]) == 2:
                     endpoints.add((i, j))
 
         pot_endpoints = one_neighbor.copy()
         for endpoint in endpoints:
-            pot_endpoints = pot_endpoints.union(self.ortho_neighbors(endpoint, board))
+            pot_endpoints = pot_endpoints.union(self.ortho_neighbors(endpoint))
 
-        return pot_endpoints
+        if pot_endpoints:
+            return pot_endpoints
+        else:
+            for row in self.temp_game:
+                for col in row:
+                    if not col:
+                        return set((row, col))
 
 
-    def extend_path(self, board, path, color):
+    def extend_path(self, path, color):
         """Randomly extends a path in any valid direction"""
-        empty_neighbors = self.ortho_neighbors(path[0], board)
+        empty_neighbors = self.ortho_neighbors(path[0])
         # if the path can be extended at its beginning
         if empty_neighbors:
-            board[path[0][0]][path[0][1]] = color
+            self.temp_game[path[0][0]][path[0][1]] = color
             sq = empty_neighbors.pop()
             path.insert(0, sq)
-            board[sq[0]][sq[1]] = color + color
+            self.temp_game[sq[0]][sq[1]] = color + color
         # if the path can be extended at its end
         else:
-            empty_neighbors = self.ortho_neighbors(path[len(path) - 1], board)
+            empty_neighbors = self.ortho_neighbors(path[len(path) - 1])
             if empty_neighbors:
-                board[path[len(path) - 1][0]][path[len(path) - 1][1]] = color
+                self.temp_game[path[len(path) - 1][0]][path[len(path) - 1][1]] = color
                 sq = empty_neighbors.pop()
                 path.append(sq)
-                board[sq[0]][sq[1]] = color + color
+                self.temp_game[sq[0]][sq[1]] = color + color
 
 
-    def fill_holes(self, board, path, cluster, color):
+    def fill_holes(self, path, cluster, color):
         """Attempts to fill the holes created by drawing a specific line"""
         while cluster:
             # seeing is cluster is neighbor to beginning or end
-            beginning_neighbors = self.ortho_neighbors(path[0], board)
-            end_neighbors = self.ortho_neighbors(path[len(path) - 1], board)
+            beginning_neighbors = self.ortho_neighbors(path[0])
+            end_neighbors = self.ortho_neighbors(path[len(path) - 1])
             beginning_overlap = cluster.intersection(beginning_neighbors)
             end_overlap = cluster.intersection(end_neighbors)
             if beginning_overlap:
                 cluster = cluster.difference(beginning_overlap)
-                board[path[0][0]][path[0][1]] = color
+                self.temp_game[path[0][0]][path[0][1]] = color
                 curr_point = beginning_overlap.pop()
-                board[curr_point[0]][curr_point[1]] = color * 2
+                self.temp_game[curr_point[0]][curr_point[1]] = color * 2
                 path.insert(0, curr_point)
             elif end_overlap:
                 cluster = cluster.difference(end_overlap)
                 end_pt = path[len(path) - 1]
-                board[end_pt[0]][end_pt[1]] = color
+                self.temp_game[end_pt[0]][end_pt[1]] = color
                 curr_point = end_overlap.pop()
-                board[curr_point[0]][curr_point[1]] = color * 2
+                self.temp_game[curr_point[0]][curr_point[1]] = color * 2
                 path.append(curr_point)
             else:
                 return False
