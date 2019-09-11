fcd73d9e87caace2d9088e2056afee7939561a7a
Kathleen McGuire <kathleen.macg@gmail.com>
2019-09-11 17:35:41 -0400

    added methods to the tree structure that return the visited squares and most recently added path

--- stack.py
+++ stack.py
@@ -8,12 +8,14 @@ class Point(object):
         self.previous = None
 
     def __eq__(self, p2):
-        return self.x == p2.x and self.y == p2.y
+        return self.x == p2.x and self.y == p2.y and self.end == p2.end
 
-class Stack(object):
+class Tree(object):
     def __init__(self, line):
         self.root = Point(line[0][0], line[0][1], 'r', True)
         self.curr_branch = self.root
+        # paths is a collection of pointers to the first endpoints of the paths
+        self.paths = []
         for i in range(1, len(line) - 1):
             self.push(Point(line[i][0], line[i][1], 'r'))
         i = len(line) - 1
@@ -31,4 +33,34 @@ class Stack(object):
 
 
     def pop(self):
+        '''Does not remove a node from the tree; moves the pointer to the current
+        node to its previous node.'''
         self.curr_branch = self.curr_branch.previous
+
+
+    def visited_sqs(self):
+        '''Returns a set of indices on the board that are filled'''
+        visited = set()
+        node = self.curr_branch
+        while (node):
+            visited.add((node.x, node.y))
+            node = node.previous
+        return visited
+
+
+    def most_recent_path(self):
+        '''Returns an array containing the indices of the most recent path
+        added to the tree, if any paths have been added.'''
+        if not self.paths:
+            return None
+
+        path = []
+        node = self.curr_branch
+        endpoint = self.paths[len(self.paths) - 1]
+        while (node != endpoint):
+            path.append((node.x, node.y))
+            node = node.previous
+
+        path.append((node.x, node.y))
+
+        return path 
