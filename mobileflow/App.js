eccb0442274a3475c553e6bfc47935e2eeaecdb7
Kathleen McGuire <kathleen.macg@gmail.com>
2019-10-11 13:47:26 -0400

    Fixed API call to flask server on my machine

--- mobileflow/App.js
+++ mobileflow/App.js
@@ -119,17 +119,18 @@ function Item(props) {
 }
 
 function Board(props) {
+
   const gameNum = props.gameNum;
   const setPage = props.setPage;
-  const [ends, setEnds] = useState([]);
-  const [colors, setColors] = useState([]);
-  const [lines, setLines] = useState([
-    [[], [], [], [], []],
-    [[], [], [], [], []],
-    [[], [], [], [], []],
-    [[], [], [], [], []],
-    [[], [], [], [], []]
-  ]);
+  let temp = [];
+  for (let i  = 0; i < 25; i++) {
+    temp.push({end: false,
+              color: 'white',
+              lines: [],
+              id: i
+            })
+  }
+  const [squares, setSquares] = useState(temp);
 
   // drawnLines will be used to roll back lines
   const [drawnLines, setdrawnLines] = useState([]);
@@ -152,171 +153,88 @@ function Board(props) {
 
   // board import
   useEffect(() => {
-    boardImport(boards[Number(gameNum) - 1]);
-    // fetchBoard();
+    // boardImport(boards[Number(gameNum) - 1]);
+    fetchBoard();
   }, []);
 
   // need to reconfigure flask app
-  // const fetchBoard = async () => {
-  //   let url = "10.0.13.200:5000/game/" + gameNum;
-  //   try {
-  //     let res = await fetch(url);
-  //     let resJSON = await res.json();
-  //     boardImport(resJSON);
-  //   }
-  //   catch (error) {
-  //     console.error(error);
-  //   }
-  // }
+  const fetchBoard = async () => {
+    let url = "http:10.0.13.200:5000/game/" + gameNum;
+    try {
+      let res = await fetch(url);
+      let resJSON = await res.json();
+      boardImport(resJSON.board);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
 
   const boardImport = (board) => {
-    let boardEnds = [];
-    let boardColors = [];
-
-    board.forEach(function(row) {
-      let rowEnds = [];
-      let rowColors = [];
-      row.forEach(function(sq) {
-        if (sq.length > 1) {
-          rowEnds.push(true);
-          switch(sq) {
+    for (let col = 0; col < 5; col++) {
+      for (let row = 0; row < 5; row++ ) {
+        if (board[row][col].length > 1) {
+          temp[row * 5 + col].end = true;
+          switch(board[row][col]) {
             case 'rr':
-              rowColors.push('red');
+              temp[row * 5 + col].color = 'red';
               break;
             case 'oo':
-              rowColors.push('orange');
+              temp[row * 5 + col].color = 'orange';
               break;
             case 'gg':
-              rowColors.push('green');
+              temp[row * 5 + col].color = 'green';
               break;
             case 'yy':
-              rowColors.push('yellow');
+              temp[row * 5 + col].color = 'yellow';
               break;
             case 'bb':
-              rowColors.push('blue');
+              temp[row * 5 + col].color = 'blue';
               break;
             case 'cc':
-              rowColors.push('cyan');
+              temp[row * 5 + col].color = 'cyan';
               break;
             case 'pp':
-              rowColors.push('purple');
+              temp[row * 5 + col].color = 'purple';
               break;
             case 'mm':
-              rowColors.push('maroon');
+              temp[row * 5 + col].color = 'maroon';
               break;
             default:
             // should not do this, means there is an error
-              rowColors.push('pink');
+              temp[row * 5 + col].color = 'pink';
           }
         }
         else {
-          rowEnds.push(false);
-          rowColors.push('white');
+          temp[row * 5 + col].end = false;
+          temp[row * 5 + col].color = 'white';
         };
-      })
-      boardEnds.push(rowEnds);
-      boardColors.push(rowColors);
-    });
-    setEnds(boardEnds);
-    setColors(boardColors);
+      }
+    };
+    setSquares(temp);
     setLoaded(true);
   }
-
-  const renderSquare = (i, j) => {
-    if (ends[i][j]) {
-      return (
-        <View
-          key={i.toString() + j.toString()}
-          style={{...styles.square}}>
-          <View
-            style={{
-              backgroundColor: colors[i][j],
-              ...styles.end
-            }}/>
-        </View>
-      )
-    };
-
-    // if a line should appear, it becomes opaque
-    let upOpacity = 0;
-    let leftOpacity = 0;
-    let centerOpacity = 0;
-    let rightOpacity = 0;
-    let downOpacity = 0;
-
-    if (lines[i][j].length != 0) {
-      let sqLines = lines[i][j];
-      // will always have center circle
-      centerOpacity = 1;
-      if (sqLines.includes('u')) upOpacity = 1;
-      if (sqLines.includes('l')) leftOpacity = 1;
-      if (sqLines.includes('r')) rightOpacity = 1;
-      if (sqLines.includes('d')) downOpacity = 1;
-    }
-
-    return (
-      <View
-        key={(i.toString() + j.toString())}
-        style={{...styles.square}}>
-          <View
-            style={{
-              ...styles.up,
-              opacity: upOpacity,
-              backgroundColor: colors[i][j]
-            }}
-          />
-          <View
-            style={{
-              ...styles.left,
-              opacity: leftOpacity,
-              backgroundColor: colors[i][j]
-            }}
-          />
-          <View
-            style={{
-              ...styles.center,
-              opacity: centerOpacity,
-              backgroundColor: colors[i][j]
-            }}
-          />
-          <View
-            style={{
-              ...styles.right,
-              opacity: rightOpacity,
-              backgroundColor: colors[i][j]
-            }}
-          />
-          <View
-            style={{
-              ...styles.down,
-              opacity: downOpacity,
-              backgroundColor: colors[i][j]
-            }}
-          />
-      </View>
-    )
-  };
 
   const handleFingerDown = (evt, gs) => {
     const { pageX, pageY } = evt.nativeEvent;
     const row = Math.floor((pageY - 2 * styles.board.margin) / styles.square.height);
     const col = Math.floor((pageX - 2 * styles.board.margin) / styles.square.width);
-    if (row >=0 && row < ends.length && col >= 0 && col < ends.length) {
+    if (row >=0 && row < 5 && col >= 0 && col < 5) {
       let currentLine = [[row, col]];
       let currDrawnLines = drawnLines;
       currDrawnLines.push(currentLine);
 
-      if (ends[row][col]) {
-        setNextColor(colors[row][col]);
+      if (squares[row * 5 + col].end) {
+        setNextColor(squares[row * 5 + col].color);
         setPrevSq([row, col]);
         setdrawnLines(currDrawnLines);
       }
-      else if (colors[row][col] != 'white') {
+      else if (squares[row * 5 + col].color != 'white') {
         // TODO: how to roll back lines?
-        let currColors = colors.slice(0);
-        currColors[row][col] = 'white';
+        let currSqs = squares.slice(0);
+        currSqs[row * 5 + col].color = 'white';
         setdrawnLines(currDrawnLines);
-        setColors(currColors);
+        setSqs(currSqs);
       }
     }
   }
@@ -325,66 +243,65 @@ function Board(props) {
     const { pageX, pageY } = evt.nativeEvent;
     const row = Math.floor((pageY - 2 * styles.board.margin) / styles.square.height);
     const col = Math.floor((pageX - 2 * styles.board.margin) / styles.square.width);
-    if (row >=0 && row < ends.length && col >= 0 && col < ends.length) {
-      let currColors = colors.slice(0);
-      let currLines = lines.slice(0);
-      if (!ends[row][col] && colors[row][col] != nextColor) {
-        currColors[row][col] = nextColor;
-        setColors(currColors);
+    if (prevSq && row >=0 && row < 5 && col >= 0 && col < 5) {
+      let currSqs = squares.slice(0);
+      if (!squares[row * 5 + col].end && squares[row * 5 + col].color != nextColor) {
+        currSqs[row * 5 + col].color = nextColor;
+        setSquares(currSqs);
       }
       if (row != prevSq[0] || col != prevSq[1]) {
         // for each new square added to the line, will be drawing either a horizontal or vertical line
-        if (currLines[prevSq[0]][prevSq[1]].length === 2) {
-          currLines[prevSq[0]][prevSq[1]].shift();
+        if (squares[prevSq[0] * 5 + prevSq[1]].lines.length === 2) {
+          currSqs[prevSq[0] * 5 + prevSq[1]].lines.shift();
         }
-        if (currLines[row][col].length === 2) {
-          currLines[row][col].shift();
+        if (squares[row * 5 + col].lines.length === 2) {
+          currSqs[row * 5 + col].lines.shift();
         }
         // line moving left
         if (col < prevSq[1]) {
-          currLines[prevSq[0]][prevSq[1]].push('l');
-          currLines[row][col].push('r');
+          currSqs[prevSq[0] * 5 + prevSq[1]].lines.push('l');
+          currSqs[row * 5 + col].lines.push('r');
         }
         // line moving right
         else if (col > prevSq[1]) {
-          currLines[prevSq[0]][prevSq[1]].push('r');
-          currLines[row][col].push('l');
+          currSqs[prevSq[0] * 5 + prevSq[1]].lines.push('r');
+          currSqs[row * 5 + col].lines.push('l');
         }
         // line moving up
         else if (row < prevSq[0]) {
-          currLines[prevSq[0]][prevSq[1]].push('u');
-          currLines[row][col].push('d');
+          currSqs[prevSq[0] * 5 + prevSq[1]].lines.push('u');
+          currSqs[row * 5 + col].lines.push('d');
         }
         // line moving down
         else if (row > prevSq[0]) {
-          currLines[prevSq[0]][prevSq[1]].push('d');
-          currLines[row][col].push('u');
+          currSqs[prevSq[0] * 5 + prevSq[1]].lines.push('d');
+          currSqs[row * 5 + col].lines.push('u');
         }
         setPrevSq([row, col]);
-        setLines(currLines);
+        setSquares(currSqs);
       }
     }
   };
 
-  const buildBoard = () => {
-    let board = [];
-    for (let i = 0; i < ends.length; i++) {
-      let children = [];
-      for (let j = 0; j < ends.length; j++) {
-        children.push(renderSquare(i, j));
-      };
-
-      board.push(<View
-                    key={i.toString()}
-                    style={{
-                      flexDirection: 'row',
-                    }}
-                  >
-                    {children}
-                  </View>)
-    }
-    return board;
-  };
+  // const buildBoard = () => {
+  //   let board = [];
+  //   for (let i = 0; i < ends.length; i++) {
+  //     let children = [];
+  //     for (let j = 0; j < ends.length; j++) {
+  //       children.push(renderSquare(i, j));
+  //     };
+  //
+  //     board.push(<View
+  //                   key={i.toString()}
+  //                   style={{
+  //                     flexDirection: 'row',
+  //                   }}
+  //                 >
+  //                   {children}
+  //                 </View>)
+  //   }
+  //   return board;
+  // };
 
   if (loaded) {
     return (
@@ -392,10 +309,21 @@ function Board(props) {
         style={{...styles.board}}
       >
         <View
-          style={{...styles.board}}
+          style={{...styles.board, flex: 0, height: 242}}
           {...pr.panHandlers}
         >
-          {buildBoard()}
+          <FlatList
+            data={squares}
+            numColumns={5}
+            renderItem={({ item }) =>
+              <Square
+                end={item.end}
+                lines={item.lines}
+                color={item.color}
+                id={item.id}
+              />}
+            keyExtractor={item => item.id}
+          />
         </View>
         <View style={{margin: 50}}>
           <Button
@@ -422,6 +350,85 @@ function Board(props) {
   )
 }
 
+function Square(props) {
+  const end = props.end;
+  const lines = props.lines;
+  const color = props.color;
+  const id = props.id;
+  if (end) {
+    return (
+      <View
+        key={id.toString()}
+        style={{...styles.square}}>
+        <View
+          style={{
+            backgroundColor: color,
+            ...styles.end
+          }}/>
+      </View>
+    )
+  };
+
+  // if a line should appear, it becomes opaque
+  let upOpacity = 0;
+  let leftOpacity = 0;
+  let centerOpacity = 0;
+  let rightOpacity = 0;
+  let downOpacity = 0;
+
+  if (lines.length != 0) {
+    // will always have center circle
+    centerOpacity = 1;
+    if (lines.includes('u')) upOpacity = 1;
+    if (lines.includes('l')) leftOpacity = 1;
+    if (lines.includes('r')) rightOpacity = 1;
+    if (lines.includes('d')) downOpacity = 1;
+  }
+
+  return (
+    <View
+      key={id.toString()}
+      style={{...styles.square}}>
+        <View
+          style={{
+            ...styles.up,
+            opacity: upOpacity,
+            backgroundColor: color
+          }}
+        />
+        <View
+          style={{
+            ...styles.left,
+            opacity: leftOpacity,
+            backgroundColor: color
+          }}
+        />
+        <View
+          style={{
+            ...styles.center,
+            opacity: centerOpacity,
+            backgroundColor: color
+          }}
+        />
+        <View
+          style={{
+            ...styles.right,
+            opacity: rightOpacity,
+            backgroundColor: color
+          }}
+        />
+        <View
+          style={{
+            ...styles.down,
+            opacity: downOpacity,
+            backgroundColor: color
+          }}
+        />
+    </View>
+  )
+}
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -433,10 +440,11 @@ const styles = StyleSheet.create({
     fontWeight: 'bold'
   },
   board: {
-    // TODO: make the placement of these squares nicer
     flex: 1,
-    margin: 30,
-    justifyContent: 'flex-start'
+    margin: 29,
+    justifyContent: 'flex-start',
+    borderWidth: 1,
+    borderColor: 'black'
   },
   square: {
     height: 50,
